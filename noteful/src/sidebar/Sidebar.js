850512f9cb5079633fdc0752085aedfa41cba001
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-28 12:12:40 -0700

    attempt first pass at Sidebar

--- noteful/src/sidebar/Sidebar.js
+++ noteful/src/sidebar/Sidebar.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Folder   from '../Folder/Folder';
+import folders from '../store/dummy-store';
+import { Route } from 'react-router-dom';
 
 export default class Sidebar extends Component{
     render(){
+
         return(
             <div>
-                <Folder />
+                {folders.map(folder =>
+                    <Route
+                        path='/FolderPage/:folderId'
+                        render={() =>
+                            <Folder
+                                id={folder.id}
+                                name={folder.name}
+                            />}
+                    />
+                )}
             </div>
-        )
+        );
     }
 
 }
