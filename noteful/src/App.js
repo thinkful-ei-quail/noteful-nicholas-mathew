0d23ef020f46c1595c7e2469bf326f8c510dc8fa
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-28 11:02:44 -0700

    add component files

--- noteful/src/App.js
+++ noteful/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import MainPage from './MainPage/MainPage';
+import FolderPage from './FolderPage/FolderPage';
+import NotePage from './NotePage/NotePage';
+import Header from '.header/Header';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component () {
+  render () {
+    return (
+      <main className='App'>
+        <header>
+          <Route path='/MainPage' component={Header} />
+        </header>
+        <Route path='/MainPage' component={MainPage} />
+        <Route path='/FolderPage' component={FolderPage} />
+        <Route path='/NotePage' component={NotePage} />
+      </main>
+    );
+  }
 }
 
 export default App;
