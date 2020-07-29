2dc7c003b6267c244e1a7271765f5be77378b64a
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-29 15:52:06 -0700

    add style so folder is highlighted when selected

--- noteful/src/FolderList/FolderList.js
+++ noteful/src/FolderList/FolderList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink} from 'react-router-dom';
 import {notesCountForFolder} from '../finder';
+import './FolderList.css';
 
 export default class FolderList extends Component{
     render(){
